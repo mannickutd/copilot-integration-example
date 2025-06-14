ebdaf3df8a485114b8e3cbb9dd7ff0121a23c393
staplesn <Nicholas.Staples@centrica.com>
2025-06-14 13:59:22 +0100

    Add CORS support for the rest API.

--- copilot_integration_example/api.py
+++ copilot_integration_example/api.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import clients, networks
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to restrict origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
